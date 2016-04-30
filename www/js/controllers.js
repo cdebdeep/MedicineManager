0dfbb2c4f75e0ff1296f5bd0397bfa2d170c9e59
cdebdeep <cdebdeep@gmail.com>
2016-04-30 19:14:08 +0530

    2nd Commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,15 +4,68 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('addMedicineDetailsCtrl', function($scope) {
-
+.controller('addMedicineDetailsCtrl', function($scope,MedicineMasterService) {
+        function init(){
+        $scope.MedicineMasterCollection=[];   
+        var TempCollection =MedicineMasterService.GetAllMedicieMaster();
+        angular.forEach(TempCollection,function(v,k){
+            $scope.MedicineMasterCollection.push(v);            
+        });
+    }   
+    
+     init();  
 })
       
-.controller('medicineMasterCtrl', function($scope) {
-
+.controller('medicineMasterCtrl', function($scope,MedicineMasterService,$ionicPopup) {
+    //MedicineMasterService.DeleteMedicieMaster();
+    function init(){
+        $scope.MedicineMasterCollection=[];   
+        var TempCollection =MedicineMasterService.GetAllMedicieMaster();
+        angular.forEach(TempCollection,function(v,k){
+            $scope.MedicineMasterCollection.push(v);            
+        });
+    }
+    
+    $scope.PostMedicieMaster=function(NewItem) {
+        $scope.MedicineMasterCollection=[];        
+        MedicineMasterService.PostMedicieMaster(NewItem);        
+        NewItem.MedicineName='';
+        NewItem.GenericName='';
+        var TempCollection =MedicineMasterService.GetAllMedicieMaster();
+        angular.forEach(TempCollection,function(v,k){
+            $scope.MedicineMasterCollection.push(v);            
+        });          
+    };
+    
+    $scope.GetAllMedicieMaster=function() {    
+         $scope.MedicineMasterCollection=[];   
+         var TempCollection =MedicineMasterService.GetAllMedicieMaster();
+        angular.forEach(TempCollection,function(v,k){
+            $scope.MedicineMasterCollection.push(v);            
+        });
+    }; 
+    
+    $scope.DeleteRecord=function($index) {
+        var confirm = $ionicPopup.confirm({
+                title:'Confirm Delete',
+                template:'Are you sure to delete !'
+                });
+                confirm.then(function(res){
+                if(res){
+                    var TempCollection = MedicineMasterService.DeleteRecord($index);
+                        $scope.MedicineMasterCollection=[];
+                        angular.forEach(TempCollection,function(v,k){
+                                $scope.MedicineMasterCollection.push(v);            
+                            });                     
+                }                
+                });     
+        
+    };  
+     
+    init();  
 })
    
 .controller('medicineStockCtrl', function($scope) {
 
-})
+});
  
--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,45 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+    .factory('BlankFactory', [function () {
 
-}])
+    }])
 
-.service('BlankService', [function(){
+    .service('MedicineMasterService', ['$localStorage','$ionicPopup', function ($localStorage,$ionicPopup) {
+        var MedicineMasterService = {};
+        $localStorage = $localStorage.$default({
+            MedicieMasterCollection: []
+        });
+        MedicineMasterService.PostMedicieMaster = function (obj) {
+            $localStorage.MedicieMasterCollection.push(
+                {
+                    MedicineName: obj.MedicineName,
+                    GenericName: obj.GenericName
+                }
+            );
+            console.log($localStorage.MedicieMasterCollection);
+            return $localStorage.MedicieMasterCollection;
+        };
+        MedicineMasterService.GetMedicieMaster = function (MedicineName) {
+            angular.forEach($localStorage.MedicieMasterCollection, function (v, k) {
+                if (v.MedicineName == MedicineName)
+                    return v;
+                else
+                    return null;
+            });
 
-}]);
+        };
+        MedicineMasterService.GetAllMedicieMaster = function () {
+            return $localStorage.MedicieMasterCollection;
+        };
+        MedicineMasterService.DeleteMedicieMaster = function () {
+            $localStorage.MedicieMasterCollection = [];
+        };
+        MedicineMasterService.DeleteRecord=function ($index) {
+            $localStorage.MedicieMasterCollection.splice($index,1);  
+            return $localStorage.MedicieMasterCollection;           
+            
+                
+  };
+        return MedicineMasterService;
+    }]);
 
